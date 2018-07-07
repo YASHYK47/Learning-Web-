a0a67e4a6c1ccb461064b436b3521ad5cdbc1e56
YASHYK47 <yashky47@gmail.com>
2018-07-08 02:36:03 +0530

    Add test cases for GET /todos/:id

--- Todo With MongoDB/server/tests/server.test.js
+++ Todo With MongoDB/server/tests/server.test.js
@@ -3,9 +3,19 @@ const request=require('supertest');
 
 const {app}=require('./../server.js');
 const {Todo}=require('./../models/todo.js');
+const {ObjectID}=require('mongodb');
 
+const todos=[{
+  _id:new ObjectID(),
+  text:'First test todo'
+},{
+  _id:new ObjectID(),
+  text:'Second test todo'
+}];
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    Todo.insertMany(todos);
+  }).then(()=>done());
 });
 describe('POST /todos',()=>{
    it('should create a new Todo',(done)=>{
@@ -22,7 +32,7 @@ describe('POST /todos',()=>{
        if(err){
          return done(err);
        }
-       Todo.find().then((todos)=>{
+       Todo.find({text}).then((todos)=>{
          expect(todos.length).toBe(1);
          expect(todos[0].text).toBe(text);
          done();
@@ -39,9 +49,47 @@ describe('POST /todos',()=>{
          return done(err);
        }
        Todo.find().then((todos)=>{
-         expect(todos.length).toBe(0);
+         expect(todos.length).toBe(2);
          done();
        }).catch((e)=>done(e));
      });
    });
+});
+
+describe('GET /todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  });
+});
+
+describe('GET /todos/:id',()=>{
+  it('should return to doc',(done)=>{
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+
+it('should return 404 if todo not found',(done)=>{
+  var abid=new ObjectID;
+  request(app)
+  .get(`/todos/${abid.toHexString}`)
+  .expect(404)
+  .end(done);
+});
+
+it('should return 404 for non object ids',(done)=>{
+  request(app)
+  .get('/todos/123')
+  .expect(404)
+  .end(done);
+});
 });
