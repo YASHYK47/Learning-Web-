84a313b108035c8b18b8b27b574eb5577b5ab0de
YASHYK47 <yashky47@gmail.com>
2018-07-08 01:09:05 +0530

    Add GET/todos/:id

--- Todo With MongoDB/server/server.js
+++ Todo With MongoDB/server/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
-
+const {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose.js');
 var {Todo}=require('./models/todo.js');
 var {Users}=require('./models/user.js')
@@ -23,8 +23,22 @@ app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
     res.send({todos});
   },(e)=>{
-  res.status(400).send(e);  
+  res.status(400).send(e);
   })
+});
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id)){
+  return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+    return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
 });
 app.listen(3000,()=>{
   console.log('Server is on port 3000');
