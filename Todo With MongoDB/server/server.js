1ab7ee7a49b903cb712109c1481e57f46ce566b1
YASHYK47 <yashky47@gmail.com>
2018-07-07 18:23:35 +0530

    Adding Test Files

--- Todo With MongoDB/server/server.js
+++ Todo With MongoDB/server/server.js
@@ -9,15 +9,26 @@ var app=express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
-  var newtodo=Todo({
+  var todo=new Todo({
     text:req.body.text
   });
-  newtodo.save().then((doc)=>{
+  todo.save().then((doc)=>{
     res.send(doc);
   },(e)=>{
     res.status(400).send(e);
   });
 });
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+  res.status(400).send(e);  
+  })
+});
 app.listen(3000,()=>{
   console.log('Server is on port 3000');
 });
+module.exports={
+app
+};
