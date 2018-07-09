29b5a1cebc835b5f5e3329e0cecfc4638f30fa73
YASHYK47 <yashky47@gmail.com>
2018-07-09 20:01:20 +0530

    Adding Test For PATCH /todod/:id

--- Todo With MongoDB/server/server.js
+++ Todo With MongoDB/server/server.js
@@ -1,6 +1,8 @@
-var express=require('express');
-var bodyParser=require('body-parser');
+const _=require('lodash');
+const express=require('express');
+const bodyParser=require('body-parser');
 const {ObjectID}=require('mongodb');
+
 var {mongoose}=require('./db/mongoose.js');
 var {Todo}=require('./models/todo.js');
 var {Users}=require('./models/user.js')
@@ -57,6 +59,27 @@ app.delete('/todos/:id',(req,res)=>{
   }).catch((e)=>{
     res.status(400).send();
   });
+});
+app.patch('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  var body=_.pick(req.body,['text','completed']);
+  if(!ObjectID.isValid(id)){
+  return res.status(404).send();
+  }
+  if(_.isBoolean(body.completed)&&body.completed){
+    body.completedAt=new Date().getTime();
+  }else{
+    body.completed=false;
+    body.completedAt=null;;
+  }
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+res.status(400).send();
+});
 });
 app.listen(port,()=>{
   console.log(`Started up at port:${port}`);
